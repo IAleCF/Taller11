9ad8af04f68dc55d967fc2f66a2b693432fe73ed
jandalay <jandalay@espol.edu.ec>
2026-07-22 14:28:20 -0500

    avanceAsiento1

--- taller-junit-cine/src/test/java/AsientoTest.java
+++ taller-junit-cine/src/test/java/AsientoTest.java
@@ -16,5 +16,23 @@ class AsientoTest {
         assertEquals("A1",asiento.getCodigo());
     }
 
+    @Test
+    @DisplayName("Caso límite, cadena de longitud cero")
+    void constructorLimite() {
+        IllegalArgumentException errorLimite = assertThrows(IllegalArgumentException.class, () -> new Asiento("", ""), "Se esperaba un error al no insertar texto");
+        assertEquals("Error", errorLimite.getMessage());
+    }
+
+    @Test
+    @DisplayName("Caso error, tipo de asiento inválido")
+    void constructorError() {
+        IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> new Asiento("A2", "EstANd4r"), "Se espera un error por ingresar mal el tipo de asiento");
+        assertEquals("Error", error.getMessage());
+    }
+
+    
+
+
+
     // TODO: Traducir los demás casos de su tabla a métodos @Test.
 }
